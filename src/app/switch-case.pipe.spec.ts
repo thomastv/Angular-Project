9a4fecf348bf37a442328d4fc95f392d84e48a56
Thomas T Vempeny <44024439+thomastv@users.noreply.github.com>
2022-09-08 15:10:29 +0530

    Delete switch-case.pipe.spec.ts

--- src/app/switch-case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SwitchCasePipe } from './switch-case.pipe';
-
-describe('SwitchCasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SwitchCasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
